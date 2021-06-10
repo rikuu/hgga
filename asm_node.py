c12dee78687891c3d4c1285ebde365f0e8d42c48
rikuu <riku.walve@gmail.com>
2021-06-11 00:31:36 +0300

    skip writing short sequences

--- asm_node.py
+++ asm_node.py
@@ -4,10 +4,12 @@ from overlap import OverlapGraph
 
 MIN_OVERLAP = 10000
 MAX_OVERHANG = 1000
+MIN_LENGTH = 1000
 
 REV = { 'A': 'T',  'T': 'A', 'G': 'C', 'C': 'G' }
 reverse_complement = lambda s: ''.join(REV[c] for c in reversed(s.upper()))
 
+# Parse fasta file to memory
 def parse_reads(filename):
     reads = {}
     with open(filename, 'r') as f:
@@ -17,12 +19,13 @@ def parse_reads(filename):
                 if name != '':
                     reads[name] = seq
                     seq = ''
-                name = line[1:].rstrip()
-                name = name.split()[0]
+                name = line[1:].rstrip().split()[0]
             else:
                 seq += line.rstrip()
+        
         if name != '':
             reads[name] = seq
+    
     return reads
 
 def construct_sequence(path, graph, reads):
@@ -32,10 +35,6 @@ def construct_sequence(path, graph, reads):
         return seq, contained, name
 
     simplified = graph.simplify_path(path)
-    
-    print(path)
-    print(simplified)
-
     for u, us, ue, strand in simplified:
         if strand == '+':
             seq += reads[u][us:ue]
@@ -47,36 +46,48 @@ def construct_sequence(path, graph, reads):
 
     return seq, contained, name
 
-def assemble(mappings, reads_file, out_file, min_overlap=MIN_OVERLAP, max_overhang=MAX_OVERHANG):
+def assemble(mappings, reads_file, out_file, min_overlap=MIN_OVERLAP, max_overhang=MAX_OVERHANG, min_length=MIN_LENGTH):
     graph, assembled = OverlapGraph.parse_paf(mappings, min_overlap=min_overlap, max_overhang=max_overhang)
     paths = graph.max_paths()
 
     reads = parse_reads(reads_file)
 
     with open(out_file, 'w') as f:
-        for i, path in enumerate(paths):
+        for path in paths:
             seq, contained_reads, name = construct_sequence(path, graph, reads)
-            if seq == '':
-                continue
-
-            f.write('>%s\n%s\n' % (name, seq))
             assembled += contained_reads
-            print(name)
+            if len(seq) > min_length:
+                f.write('>%s\n%s\n' % (name, seq))
 
-        # TODO: Actually find contained reads, maybe?
         for n, s in reads.items():
             if n in assembled:
                 continue
 
-            print('D: %s not contained' % n)
+            if len(s) < min_length:
+                continue
+
+            # print('D: %s not contained' % n, file=sys.stderr)
             f.write('>%s\n%s\n' % (n, s))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: %s <mappings> <reads>" % sys.argv[0], file=sys.stderr)
+    if len(sys.argv) < 3 or len(sys.argv) > 6:
+        print("Usage: %s <mappings> <reads> [min overlap] [max overhang] [min length]" % sys.argv[0], file=sys.stderr)
         sys.exit(1)
 
     mappings = sys.argv[1]
     reads = sys.argv[2]
+
+    min_overlap = MIN_OVERLAP
+    if len(sys.argv) >= 4:
+        min_overlap = int(sys.argv[3])
+
+    max_overhang = MAX_OVERHANG
+    if len(sys.argv) >= 5:
+        max_overhang = int(sys.argv[4])
+
+    min_length = MIN_LENGTH
+    if len(sys.argv) == 6:
+        min_length = int(sys.argv[5])
     
-    assemble(mappings, reads, reads + '.asm.fa')
+    assemble(mappings, reads, reads + '.asm.fa',
+        min_overlap=min_overlap, max_overhang=max_overhang, min_length=min_length)
