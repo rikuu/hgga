cbd823fb60db2e87fa470e00030206971910a8cc
rikuu <riku.walve@gmail.com>
2021-06-13 07:44:25 +0300

    fix argument parsing

--- hgga.py
+++ hgga.py
@@ -29,10 +29,10 @@ FINAL_MIN_OVERLAP = 1000
 FINAL_MAX_OVERHANG = 100
 FINAL_MIN_LENGTH = 5000
 
-if len(sys.argv) == 4:
+if len(sys.argv) >= 4:
     THREADS = int(sys.argv[3])
 
-if len(sys.argv) == 5:
+if len(sys.argv) >= 5:
     MIN_LEAF_SIZE = float(sys.argv[4])
 
 topology = leafify.leafify(READS, ALIGNMENTS, MIN_LEAF_SIZE, BIN_SIZE)
